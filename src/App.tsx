cfbf46a56ebd6a1ce00fa4666efdc6d549938f04
OnurhanTalan <o.talan2021@gtu.edu.tr>
2025-10-18 22:52:46 +0300

    chore: add chakra UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,20 @@
-import './App.css'
+import { Badge, Box, Heading } from '@chakra-ui/react';
 
 function App() {
-
   return (
-    <>
-      <h1>Personal Library Management System</h1>
-    </>
-  )
+    <Box
+      display="flex"
+      alignItems="center"
+      justifyContent="center"
+      height="100vh"
+      bg="gray.50"
+    >
+      <Badge>
+        <Heading size="5xl">Personal Library Management System</Heading>
+      </Badge>
+    </Box>
+  );
 }
 
-export default App
+export default App;
+
